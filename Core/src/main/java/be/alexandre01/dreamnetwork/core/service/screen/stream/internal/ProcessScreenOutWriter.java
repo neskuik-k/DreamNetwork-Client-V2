176430a60268ea71a9cfa2cddb68c0ae9271b8f7
Alexandre01Dev <altata01@hotmail.com>
2023-10-21 23:52:30 +0200

    Plugin update

--- Core/src/main/java/be/alexandre01/dreamnetwork/core/service/screen/stream/internal/ProcessScreenOutWriter.java
+++ Core/src/main/java/be/alexandre01/dreamnetwork/core/service/screen/stream/internal/ProcessScreenOutWriter.java
@@ -91,8 +91,6 @@ public class ProcessScreenOutWriter implements IScreenOutWriter {
     @Override
     public void writeOnConsole(String data) throws IOException {
         //Writer writer = new OutputStreamWriter(screen.getService().getProcess().getOutputStream());
-        Console.debugPrint(screen.getService().getProcess().getOutputStream());
-
         OutputStream writer = screen.getService().getProcess().getOutputStream();
         writer.write((data + "\n").getBytes());
         writer.flush();
