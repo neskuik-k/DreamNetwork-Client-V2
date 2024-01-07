3da5581f6f3e14e34e8ac98b2931aa17b3fcaf55
Alexandre01Dev <altata01@hotmail.com>
2024-01-07 10:37:52 +0100

    nogui patch

--- API/src/main/java/be/alexandre01/dreamnetwork/api/service/ConfigData.java
+++ API/src/main/java/be/alexandre01/dreamnetwork/api/service/ConfigData.java
@@ -27,7 +27,7 @@ public class ConfigData implements Serializable {
     protected IExecutor.Mods type;
     protected String xms;
     @Getter
-    protected String startup = "%java% -Xms%xms% -Xmx%xmx% -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 %args% -jar %exec% --nogui";
+    protected String startup = "%java% -Xms%xms% -Xmx%xmx% -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 %args% -jar %exec% nogui";
     protected String executable = "exec";
     @Getter protected String xmx;
     protected String javaVersion = "default";
