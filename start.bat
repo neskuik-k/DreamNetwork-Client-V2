4992445a4947086e0a81e6322d1e5eda79a2a105
Alexandre01 <34719450+Alexandre01Dev@users.noreply.github.com>
2024-12-28 13:27:10 +0100

    Delete start.bat

--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\Java\jdk-16.0.2\bin\java" -Debug=true --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/jdk.internal.ref=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED -jar .\target\DreamNetworkV2-1.7.0-SNAPSHOT-withShadedDependencies.jar
-PAUSE
